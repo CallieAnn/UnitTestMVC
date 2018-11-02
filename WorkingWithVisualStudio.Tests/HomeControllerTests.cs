17dc0479c21b7c73d5c1b9d64d603c6936c6defd
CallieAnn <dedorec@my.lanecc.edu>
2018-11-02 16:02:49 -0700

    ready to test homecontroller

---
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+    }
+}
--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -4,9 +4,20 @@ using System.Linq;
 
 namespace WorkingWithVisualStudio.Controllers {
     public class HomeController : Controller {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
 
         public IActionResult Index()
-            => View(SimpleRepository.SharedRepository.Products
+            => View(Repository.Products
                         .Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
